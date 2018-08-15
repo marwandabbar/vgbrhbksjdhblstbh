9cf8cc39063e1258cf18be631529a04f58c03d1e
marwandabbar <42333970+marwandabbar@users.noreply.github.com>
2018-08-15 07:25:27 +0100

    Update army.js

--- army.js
+++ army.js
@@ -4,7 +4,7 @@ const prefix = ','
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`,help`,"https://www.twitch.tv/youssef_tube")
+client.user.setGame(`,help`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
